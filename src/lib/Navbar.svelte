10f1b846eb9ebe5a09beb28f6a63600494f55692
Elliot <me@robiot.dev>
2022-10-13 20:42:16 +0300

    Init

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,3 @@
+<nav class="w-full bg-slate-200 p-11">
+   <div class="flex gap-2"></div>
+</nav>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import "../styles/app.css";
+    
+    import Navbar from "$lib/Navbar.svelte";
+</script>
+
+<div>
+    <Navbar />
+    <slot />
+</div>
